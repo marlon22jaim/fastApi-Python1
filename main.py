5e34a9c2e9e04e9d28f9b3db20ce99fb1dd96619
Marlon Jaimes <andres_jr19@hotmail.com>
2022-10-20 13:49:49 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
+# Python
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# FastAPI
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+
+# Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None  # null
+    is_married: Optional[bool] = None
 
 
 # path operation decorator
 @app.get("/")
 # path operation function
 def home():
-    return {"hello": "World"}
+    return {"hello": "World"}  # JSON
+
+
+# Request and Response Body
+@app.post("/person/new")
+def create_person(persona:  Person = Body()):
+    return {"Response": "Success",
+            "Code": 200,
+            "Datos_persona": persona}
